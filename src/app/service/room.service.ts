b20af59b4122f5aa988373a0619e8537db172979
orgin <lantung.2k@gmail.com>
2022-04-19 17:06:18 +0700

    ....

--- src/app/service/room.service.ts
+++ src/app/service/room.service.ts
@@ -20,4 +20,16 @@ export class RoomService {
   createRoomHost(data: any): Observable<any> {
     return this.http.post<any>(environment.url_api + `user/room`, data);
   }
+
+  updateRoomHost(id, data): Observable<any> {
+    return this.http.put<any>(environment.url_api + `user/room/${id}`, data);
+  }
+
+  detailRoomHost(id): Observable<any> {
+    return this.http.get<any>(environment.url_api + `user/room/${id}`);
+  }
+
+  deleteRoomHost(id): Observable<any> {
+    return this.http.delete<any>(environment.url_api + `user/room/${id}`);
+  }
 }
