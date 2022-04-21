a1e8953826fbf29d4599941d28077efda239081f
Hùng Phạm <hungpm.works@gmail.com>
2022-04-21 10:54:04 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,21 @@ export const environment = {
   production: false,
   url_api: "http://127.0.0.1:8000/api/",
   firebaseConfig: {
-    apiKey: "AIzaSyB7x4dURWETpFzqfYcDaMAgFil29euqZR8",
-    authDomain: "htstay-57f5e.firebaseapp.com",
-    projectId: "htstay-57f5e",
-    storageBucket: "htstay-57f5e.appspot.com",
-    messagingSenderId: "641239443693",
-    appId: "1:641239443693:web:f05547f669324ad6b004e4",
-    measurementId: "G-3E4M9D6K3G"
+    // apiKey: "AIzaSyB7x4dURWETpFzqfYcDaMAgFil29euqZR8",
+    // authDomain: "htstay-57f5e.firebaseapp.com",
+    // projectId: "htstay-57f5e",
+    // storageBucket: "htstay-57f5e.appspot.com",
+    // messagingSenderId: "641239443693",
+    // appId: "1:641239443693:web:f05547f669324ad6b004e4",
+    // measurementId: "G-3E4M9D6K3G"
+    apiKey: "AIzaSyDK-oZB7KKtB7DEzUI6dMIZd0mchR9_32w",
+    authDomain: "htstay-9727f.firebaseapp.com",
+    databaseURL: "https://htstay-9727f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "htstay-9727f",
+    storageBucket: "htstay-9727f.appspot.com",
+    messagingSenderId: "261725884910",
+    appId: "1:261725884910:web:f0ad24f24582dee1fb7eff",
+    measurementId: "G-KRNLGWJB4T"
   }
 };
 
